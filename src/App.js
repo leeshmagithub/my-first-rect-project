d16f4aea840c68a710aa13febf370cfa95752b9c
leeshmababygithub <leeshmababy@gmail.com>
2022-01-04 23:44:53 +0530

    MapFIlter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import { NavLink, Route, Routes, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
-import ContactUs from "./components/ContactUs";
-import Home from "./components/Home";
-import Login from "./components/Login";
 
+const colors = ["green", "red", "blue", "violet", "pink"];
+
+const array = colors.map((el) => {
+  return { color: el };
+});
+
+const filteredArray = colors
+  .filter((el) => {
+    return el.includes("e");
+  })
+  .map((el1) => {
+    return { color: el1 };
+  });
+
+console.log(array);
+console.log(filteredArray);
 function App() {
-  const navigate = useNavigate();
-
-  function navigateHandler() {
-    navigate("/Login");
-  }
-
   return (
     <div>
-      <NavLink to="/Home">Home</NavLink>
-      <br></br>
-      <NavLink to="/About">About</NavLink>
-      <br></br>
-      <NavLink to="/ContactUS">ContactUS</NavLink>
-      <br></br>
-      <button onClick={navigateHandler}>login</button>
-      <Routes>
-        <Route path="/Home" element={<Home />}></Route>
-        <Route path="/About" element={<About />}></Route>
-        <Route path="/ContactUS" element={<ContactUs />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-      </Routes>
+      {array.map((el) => {
+        return <div key={el.color} style={{ color: el.color }}>{el.color}</div>;
+      })}
     </div>
   );
 }
